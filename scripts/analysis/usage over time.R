9c3386539b15e20343dcfc7c6bfcd456cbb4ab5b
jberesford-fe <Justin.Beresford@frontier-economics.com>
2024-11-14 19:38:19 +0000

    removing theme to let it take from main script

--- scripts/analysis/usage over time.R
+++ scripts/analysis/usage over time.R
@@ -1,22 +1,3 @@
-custom_theme <- theme(
-  axis.text.x = element_text(
-    angle = 45,
-    hjust = 1,
-    size = 14
-  ),
-  axis.text.y = element_text(size = 14),
-  axis.title.x = element_text(size = 16),
-  axis.title.y = element_text(size = 16),
-  panel.grid.major.y = element_line(color = "lightgray"),
-  panel.grid.major.x = element_blank(),
-  panel.grid.minor = element_blank(),
-  axis.line = element_line(color = "black", size = 0.5),
-  legend.title = element_text(size = 14),
-  legend.text = element_text(size = 15),
-  legend.key.size = unit(1.5, "lines")
-)
-
-
 file_path <- file.path(dir_raw,
                        "daiquery-FB time spent (video) in UK-2024-10-07 10_33pm.xlsx")
 fb_data <- read_excel(file_path)
